07ae6db1c81a30323276c01f14218ed536a1b5d8
khthoi <pvpgammingpro@gmail.com>
2024-02-06 16:20:35 +0700

    last

--- web3/cmd3.js
+++ web3/cmd3.js
@@ -21,30 +21,3 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     })
 })
-var img = [];
-        var curimg = 0;
-        function loadImages() {
-            for (var i = 1; i < 4; i++) {
-                img[i] = new Image();
-                img[i].src = "web3/anhcanhan" + i + ".jpg";
-            }
-        }
-        function next() {
-            curimg = (curimg + 1) % img.length;
-            document.getElementById("anh").style.opacity = 0;
-            setTimeout(function () {
-                document.getElementById("anh").src = img[curimg].src;
-                document.getElementById("anh").style.opacity = 1;
-            }, 500);
-        }
-
-        function back() {
-            curimg = (curimg - 1 + img.length) % img.length;
-            document.getElementById("anh").style.opacity = 0;
-            setTimeout(function () {
-                document.getElementById("anh").src = img[curimg].src;
-                document.getElementById("anh").style.opacity = 1;
-            }, 500);
-        }
-        loadImages();
-        document.getElementById("anh").src = img[curimg].src;
